74394f5e23e2235a6617399939590d2b92596610
jngcii <concotree@gmail.com>
2020-06-12 16:22:22 +0900

    Register models in admin site

--- api/movies/admin.py
+++ api/movies/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Movie, Genre, Actor, Director
 
-# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'sub_title',
+        'poster',
+        'rating',
+        'pub_date',
+        'runtime',
+        'audits',
+        'audi_cnt',
+    ]
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Director)
